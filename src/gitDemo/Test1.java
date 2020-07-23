137786742ba166bf8eff062170fc59249118fc53
Administrator <Administrator@Khagen>
2020-07-23 17:02:51 +0100

    First commit

---
+++ src/gitDemo/Test1.java
@@ -0,0 +1,17 @@
+package gitDemo;
+
+public class Test1 {
+	
+	public static void main(String[] args) {
+		String a="apple";
+		String b="banana";
+		
+		if(a.equals(b)) {
+			System.out.println("They are same fruits");
+			}
+		else {
+			System.out.println("They are not same fruits");
+		}
+	}
+
+}
